96f1031d615eb57aa595ce0cae3e81328d7f575f
Capi <vand21130@gmail.com>
2026-06-07 18:55:25 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,5 @@
 // File: api/chat.js
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
